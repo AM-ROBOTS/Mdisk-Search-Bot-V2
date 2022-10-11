6fc8e0a58f928c63c28dd0612a0c74d57c41ef6d
AM-ROBOTS <115565191+AM-ROBOTS@users.noreply.github.com>
2022-10-12 02:00:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clonehttps://github.com/8769ANURAG/t.git /mdisk-search-bot
-cd /mdisk-search-bot
+git clone https://github.com/AM-ROBOTS/Mdisk-Search-Bot-V2.git /Mdisk-Search-Bot-V2
+cd /Mdisk-Search-Bot-V2
 pip3 install -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot....ANURAG"
 python3 main.py
