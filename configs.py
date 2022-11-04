eba0090df1e57dccf3c08d2963b40fd64e676305
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2022-11-04 22:24:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,19 +1,22 @@
-#@AM_ROBOTS
+# (c) @AM_ROBOTS
 
 import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 7588344))
-    API_HASH = os.environ.get("API_HASH", "6ade175bcf8ea4981e3153ef48fe07f5")
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL"),( None)
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
+    
+    
     
 🤖 My Name: <a href='https://t.me/Official_Movies_Group'>Mdisk Search Robot</a>
 
@@ -27,30 +30,27 @@ class Config(object):
 """
 
     ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/Am_RoBots'>ᎯℕUℛᎯᎶ</a>
-    
+
 If You Want Your Own Bot Like This Then You Can Contact Our Developer.</b>
 """
 
     HOME_TEXT = """
-    
 <b>Hey! {}😅,
 
 I'm Mdisk Search Robot.🤖</a>
 
-I Can Search!🔍 What You Want?😜
+I Can Search 🔍 What You Want❗
 
 <a>Made With ❤ By @Am_RoBots</a></b>
 """
 
 
     START_MSG = """
-    
 <b>Hey! {}😅,
 
 I'm Mdisk Search Robot.🤖</a>
 
-I Can Search!🔍 What You Want?😜
+I Can Search 🔍 What You Want❗
 
 <a>Made With ❤ By @Am_RoBots</a></b>
 """
-    
